7b2f27ed2c4d159299b56b45bae8c3c57bbf5ed3
libitong <libitong.lbt@sunyur.com>
2020-11-24 21:07:12 +0800

    提交

--- src/main/java/com/lbt/collection/Main.java
+++ src/main/java/com/lbt/collection/Main.java
@@ -12,46 +12,38 @@ public class Main {
      * 输入y，找到一个数x，x的平方接近y
      */
     public static int  find(int y){
-        int low = 1;
-        int high = y;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            // int product = mid * mid;   越界
-            int t = y / mid;
-            if (t == mid) {
-                if (y % mid == 0) {
-                    return t;
-                }
-                low = mid + 1;
-            } else if (t < mid) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        //定义返回值
+        int x ;
+        //是否二分标志位
+        boolean a = true;
+        int mid = y;
+        while(true){
+             if (a){
+                 mid = mid/2;
+             }
+             int p = mid*mid;
+            if (p==y){
+                x = mid;
+                break;
+            }
+             if ( a && p > y){
+                 continue;
+             }
+
+             if (p<y){
+                 a = false;
+                mid++;
+             }
+            if (p > y){
+                x = mid-1;
+                break;
             }
         }
-
-        return false;
+        return x;
     }
 
-    //给定一个正整数 num，编写一个函数，如果 num 是一个完全平方数，则返回 True，否则返回 False。
-    public boolean isPerfectSquare(int num) {
-        int low = 1;
-        int high = num;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            // int product = mid * mid;   越界
-            int t = num / mid;
-            if (t == mid) {
-                if (num%mid == 0) return true;
-                low = mid + 1;
-            } else if (t < mid) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return false;
+    public static void main(String[] args) {
+        System.out.println(find(81));
     }
 
 }
